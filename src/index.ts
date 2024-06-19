c0ccf2f6428d5f86a1d0df7c28d2847f1b7459b1
SoftwareX Plus <119097812+xcfio@users.noreply.github.com>
2024-06-19 20:40:27 +0600

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,40 @@
+import { json, urlencoded } from "express"
+import { lookup } from "dns"
 import express from "express"
 import cors from "cors"
-import { lookup } from "dns"
 
-const app = express().use(cors({ optionsSuccessStatus: 200 }))
+const app = express().use(json(), urlencoded({ extended: false }), cors({ optionsSuccessStatus: 200 }))
+
+const map = new Map<number, string>()
+let current_count = 0
 
 app.get("/", (_req, res) => res.send("Hello World"))
-app.get("/api/whoami", async (req, res) => {
-    lookup("xcf", (error, address) => {})
-    res.json({
-        ipaddress: req.ip,
-        software: req.headers["user-agent"],
-        language: req.headers["accept-language"]
+
+app.post("/api/shorturl", async (req, res) => {
+    const url = (req.body.url_input as string | undefined) ?? (req.body.url as string | undefined) ?? ""
+
+    lookup(url, (error) => {
+        if (error) return res.status(400).json({ error: "Invalid URL" })
+        const url_map = new Map(Array.from(map.entries()).map((value) => [value[1], value[0]]))
+
+        if (url_map.has(url)) {
+            res.json({ original_url: url, short_url: url_map.get(url) })
+        } else {
+            const short_url = current_count++
+            map.set(short_url, url)
+            res.json({ original_url: url, short_url })
+        }
     })
+})
+
+app.get("/api/shorturl/:code?", (req, res) => {
+    const code = parseInt(req.params.code ?? "")
+    if (isNaN(code)) return res.status(400).json({ error: "Wrong format" })
+
+    const original_url = map.get(code)
+    if (!original_url) return res.status(400).json({ error: "No short URL found for the given input" })
+
+    res.redirect(original_url)
 })
 
 app.listen(3000, () => console.log("Server is running"))
