51159044c581517cd3debcf5dd2d108bb34e1fba
SoftwareX Plus <119097812+xcfio@users.noreply.github.com>
2024-06-20 08:44:16 +0600

    add logic

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,49 @@
-import { json, urlencoded } from "express"
-import { lookup } from "dns"
+import { urlencoded } from "express"
+import { lookup } from "node:dns"
+import { URL } from "node:url"
 import express from "express"
 import cors from "cors"
 
-const app = express().use(json(), urlencoded({ extended: false }), cors({ optionsSuccessStatus: 200 }))
+const app = express().use(urlencoded({ extended: false }), cors({ optionsSuccessStatus: 200 }))
 
 const map = new Map<number, string>()
 let current_count = 0
 
 app.get("/", (_req, res) => res.send("Hello World"))
 
-app.post("/api/shorturl", async (req, res) => {
-    const url = (req.body.url_input as string | undefined) ?? (req.body.url as string | undefined) ?? ""
+app.post("/api/shorturl", (req, res) => {
+    try {
+        const url = new URL((req.body.url as string | undefined) ?? "")
 
-    lookup(url, (error) => {
-        if (error) return res.status(400).json({ error: "Invalid URL" })
-        const url_map = new Map(Array.from(map.entries()).map((value) => [value[1], value[0]]))
+        lookup(url.hostname, (error) => {
+            if (error) return res.json({ error: "Invalid URL" })
 
-        if (url_map.has(url)) {
-            res.json({ original_url: url, short_url: url_map.get(url) })
+            const url_map = new Map(Array.from(map.entries()).map((value) => [value[1], value[0]]))
+
+            if (url_map.has(url.href)) {
+                res.json({ original_url: url, short_url: url_map.get(url.href) })
+            } else {
+                const short_url = current_count++
+                map.set(short_url, url.href)
+                res.json({ original_url: url, short_url })
+            }
+        })
+    } catch (error) {
+        if (error instanceof Error && error.message === "Invalid URL") {
+            if (error) return res.json({ error: "Invalid URL" })
         } else {
-            const short_url = current_count++
-            map.set(short_url, url)
-            res.json({ original_url: url, short_url })
+            console.trace(error)
+            res.sendStatus(500)
         }
-    })
+    }
 })
 
 app.get("/api/shorturl/:code?", (req, res) => {
     const code = parseInt(req.params.code ?? "")
-    if (isNaN(code)) return res.status(400).json({ error: "Wrong format" })
+    if (isNaN(code)) return res.json({ error: "Wrong format" })
 
     const original_url = map.get(code)
-    if (!original_url) return res.status(400).json({ error: "No short URL found for the given input" })
+    if (!original_url) return res.json({ error: "No short URL found for the given input" })
 
     res.redirect(original_url)
 })
